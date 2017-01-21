961e509e1e82c355cd18937057f31e6651fde8ed
Ultime 5528 <keranna.first@gmail.com>
2017-01-21 14:43:28 -0500

    Correction commande SShootOnce et Valeur Dashboard

--- src/Commands/SShootOnce.cpp
+++ src/Commands/SShootOnce.cpp
@@ -19,7 +19,7 @@ void SShootOnce::Execute() {
 
 // Make this return true when this Command no longer needs to run execute()
 bool SShootOnce::IsFinished() {
-	return true;
+	return false;
 }
 
 // Called once after isFinished returns true
